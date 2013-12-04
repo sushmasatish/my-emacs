d6ebbe228f367fbc84b7ee79fec03444e975fd5c
Bruno Bonacci <bruno.bonacci@gmail.com>
2013-12-04 17:35:30 +0000

    Add more natural <up> and <down> key bindings for nrepl and shell mode

--- config/bindings.el
+++ config/bindings.el
@@ -3,3 +3,23 @@
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+
+;; Add more natural <up> and <down> key bindings for shell mode
+(defun my-shell-mode-keys ()
+  "Modify keymaps used by repl."
+  (local-set-key (kbd "<up>")   'comint-previous-input)
+  (local-set-key (kbd "<down>") 'comint-next-input)
+  )
+
+(add-hook 'shell-mode-hook 'my-shell-mode-keys)
+
+
+;; Add more natural <up> and <down> key bindings for nrepl mode
+(defun my-nrepl-mode-keys ()
+  "Modify keymaps used by repl."
+  (local-set-key (kbd "<up>")   'nrepl-previous-input)
+  (local-set-key (kbd "<down>") 'nrepl-next-input)
+  )
+
+(add-hook 'nrepl-mode-hook 'my-nrepl-mode-keys)
